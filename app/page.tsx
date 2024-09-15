83e385f7ab90a60cd901c9039967e7dfe2374a96
Matvey Ponomarew <ponomarew.matv@yandex.ru>
2024-09-15 16:12:35 +0300

    Сверстал title и categories

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import { Button } from '@/components/ui/button'
+import { Categories, Container, Title } from '@/components/shared'
 
 export default function Home() {
   return (
-    <h1>
-      Главная
-    </h1>
+    <>
+      <Container className='mt-10'>
+        <Title text='Все пиццы' size='lg' className='font-extrabold' />
+        <Categories />
+      </Container>
+    </>
   );
 }
