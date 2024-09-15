8e1efa0327a6b1a176ede1b7487ee325cf397007
Matvey Ponomarew <ponomarew.matv@yandex.ru>
2024-09-15 19:33:52 +0300

    сверстал sort-popup и topbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
-import { Categories, Container, Title } from '@/components/shared'
+import { Categories, Container, SortPopup, Title, TopBar } from '@/components/shared'
+
 
 export default function Home() {
   return (
     <>
       <Container className='mt-10'>
         <Title text='Все пиццы' size='lg' className='font-extrabold' />
-        <Categories />
       </Container>
+      <TopBar />
     </>
   );
 }
